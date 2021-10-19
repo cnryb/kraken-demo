57b547db3d2ec29def6bb2f153f45b11842f22b3
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-10-19 18:05:22 +0800

    test case 1

---
+++ test-case/test-case1/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<!-- 这个是下面代码在浏览器中代码的样子 -->
+  <div><h3 style="background-color: #666;">这个是标题</h3><div style="height: 100px; background-color: #999; margin-top: 100px;" /></div>
+
+<!-- 这种写法会出现异常 -->
+  <card>
+    <div style="height: 100px; backgroundColor: #999; marginTop: 100px" />
+  </card>
+</template>
+
+<script setup>
+import Card from './Card.vue'
+
+</script>
---
+++ test-case/test-case1/src/Card.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h3 style="background-color: #666;">这个是不正确的标题</h3>
+    <slot />
+  </div>
+</template>
