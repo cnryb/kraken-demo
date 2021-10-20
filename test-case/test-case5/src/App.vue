08cb98cebdcc1b925680605666e531a862341269
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-10-20 18:34:12 +0800

    test-case5

---
+++ test-case/test-case5/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- 这段展示异常正常 -->
+
+<div style="display: flex; justify-content: space-between; align-items: center;">
+  <div
+    v-for="color in 3"
+    :key="color"
+    style="border: 1px solid red;"
+  >{{ color }}</div>
+</div>
+
+<!-- 这段正常 -->
+<div style="display: flex; justify-content: space-between; align-items: center;">
+    <div style="border: 1px solid red;">1</div>
+    <div style="border: 1px solid red;">2</div>
+    <div style="border: 1px solid red;">3</div>
+</div>
+</template>
+
