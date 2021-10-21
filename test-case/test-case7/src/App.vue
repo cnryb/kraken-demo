99652616655ab1a92714e0ca9fc3384fad8f5eee
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-10-21 15:42:10 +0800

    test-case7

---
+++ test-case/test-case7/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="position: relative;
+            width: 100px;
+            height: 100px;
+            backgroundColor: lightblue;">
+    相对定位
+  </div>
+
+  <div style="position: sticky;
+            top: 100px;
+            left: 100px;
+            width: 100px;
+            height: 100px;
+            backgroundColor: mediumpurple;
+            marginTop: 40px">
+            <!-- 在浏览器中生效的是 marginTop ，会无视掉 top 的值。
+            在 kraken 中，生效的似乎是 top 的值，当值大于 100px 时会生效 -->
+    粘性定位
+  </div>
+</template>
