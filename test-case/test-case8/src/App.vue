cff40e61908828acd9567f7e198a2c0d2572d2c9
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-10-21 18:33:21 +0800

    test-case8

---
+++ test-case/test-case8/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="position: sticky; top: 0px; background: #fff; display: flex; ">
+    <h1>布局</h1>
+  </div>
+  <div style="background-color: #efefef; box-sizing: border-box;">
+    <div v-for="i in 300"
+         :key="i">{{ i }}</div>
+  </div>
+</template>
