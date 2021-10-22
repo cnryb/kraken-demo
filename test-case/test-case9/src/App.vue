2a1a20589505dc40fb73d4732d3cc632aad1fcc4
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-10-22 15:22:03 +0800

    test-case9

---
+++ test-case/test-case9/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- sticky 对象设置了 overflow:hidden; 以后失去粘性定位能力。删除后即可回复正常。 -->
+  <div>
+    <div style="position: sticky;  overflow:hidden; top: 56px; background-color: #666">A</div>
+    <div v-for="i in 300" :key="i">{{ i }}</div>
+  </div>
+</template>
