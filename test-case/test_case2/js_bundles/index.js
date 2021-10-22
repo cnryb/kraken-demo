71bb3f4d622b385689d18f2e7879ea3dd44c4ed1
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-10-22 12:30:19 +0800

    post 拦截测试

--- test-case/test_case2/js_bundles/index.js
+++ test-case/test_case2/js_bundles/index.js
@@ -1,20 +1,29 @@
 
-const btn = document.createElement('button');
-btn.appendChild(document.createTextNode('点击这里'))
-btn.onclick = async () => {
-    const response = await fetch('https://fe.zhaopin.com/data-normalization/api/general', {
+// const btn = document.createElement('button');
+// btn.appendChild(document.createTextNode('点击这里'))
+// btn.onclick = async () => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts/', {
+//         method: 'POST',
+//         body: JSON.stringify({ "name": "kraken" }),
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     })
+//     const value = await response.json()
+//     console.log(value)
+// }
+
+// document.body.appendChild(btn);
+
+
+(async function () {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/', {
         method: 'POST',
-        body: JSON.stringify([{ "dictName": "sex" }]),
+        body: JSON.stringify({ "name": "kraken" }),
         headers: {
             'Content-Type': 'application/json',
         },
     })
-    console.log(response)
     const value = await response.json()
-    console.log('---------')
-
     console.log(value)
-}
-
-document.body.appendChild(btn);
-
+})();
--- test-case/test_case2/lib/main.dart
+++ test-case/test_case2/lib/main.dart
@@ -1,8 +1,66 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:kraken/kraken.dart';
+import 'dart:typed_data';
+import 'package:kraken/foundation.dart';
 
 void main() {
-  runApp(MyApp());
+    runApp(Kraken(
+    bundleContent: '''
+(async function () {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/', {
+        method: 'POST',
+        body: JSON.stringify({ "name": "kraken" }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    const value = await response.json()
+    console.log(value)
+})();
+    ''',
+    httpClientInterceptor: CustomHttpClientInterceptor(),
+  ));
+}
+
+
+
+class CustomHttpClientInterceptor implements HttpClientInterceptor {
+  DateTime? _startTime;
+  DateTime? _endTime;
+
+  @override
+  Future<HttpClientRequest?> beforeRequest(HttpClientRequest request) async {
+    request.headers.set('x-foo', 'bar');
+    print('beforeRequest, request headers: ${request.headers}');
+    if (request.uri.path == '/posts/') {
+      _startTime = DateTime.now();
+    }
+    return null;
+  }
+
+  @override
+  Future<HttpClientResponse?> afterResponse(HttpClientRequest request, HttpClientResponse response) async {
+    print('afterResponse, response headers: ${response.headers}');
+    if (request.uri.path == '/posts/') {
+      _endTime = DateTime.now();
+      print('/posts/ cost: ${_endTime!.millisecondsSinceEpoch - _startTime!.millisecondsSinceEpoch}ms');
+    }
+    return null;
+  }
+
+  @override
+  Future<HttpClientResponse?> shouldInterceptRequest(HttpClientRequest request) async {
+    if (request.uri.path == '/posts/') {
+      // Direct output string, transform to stream.
+      String replaced = '{ "foo": "bar" }';
+      Uint8List data = Uint8List.fromList(replaced.codeUnits);
+      Stream<Uint8List> stream = Stream<Uint8List>.value(data);
+      return HttpClientStreamResponse(stream, responseHeaders: { 'x-kraken': 'hey', 'hello': 'world' });
+    }
+    return null;
+  }
 }
 
 class MyApp extends StatelessWidget {
