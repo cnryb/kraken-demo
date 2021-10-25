c75a5a134c1eae9496b242b46ed32ae03a613c47
任跃兵 <cnryb@live.cn>
2021-10-25 21:36:56 +0800

    test-case11

---
+++ test-case/test-case11/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="max-width: 100%;
+      margin: 40px;
+      border: 1px solid red;">
+    Kraken is a high-performance rendering engine based on W3C standards. Kraken's bottom layer is rendered based on Flutter, and its self-drawn rendering feature ensures multi-end consistency. The upper layer is implemented based on W3C standards and has a very large front-end developer ecosystem.
+  </div>
+</template>
