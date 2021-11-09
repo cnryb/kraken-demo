c541a71268c5a17a5091ff50a7faf3dfb975a817
任跃兵 <yuebing.ren@zhaopin.com.cn>
2021-11-09 17:51:35 +0800

    test-case12

---
+++ test-case/test-case12/README.md
@@ -0,0 +1,4 @@
+# vue 
+
+1、安装依赖
+2、yarn open 使用 kraken 打开展示
---
+++ test-case/test-case12/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="toggle">toggle visibility</button>
+
+  <div>fuck vShow: {{ show }}</div>
+  <div v-if="show" style="padding: 20px; background: red; color: white">content</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const show = ref(false)
+
+function toggle() {
+  show.value = !show.value
+}
+</script>
